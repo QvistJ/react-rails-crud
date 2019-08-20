3d1885b02be2ff42ef970fd9fe67b52e5f2e244f
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2019-08-20 19:32:58 +0200

    Setup rest api, and add webpacker

---
+++ app/controllers/api/events_controller.rb
@@ -0,0 +1,47 @@
+class Api::EventsController < ApplicationController
+  respond_to :json 
+
+  def index
+    respond_with Event.order(event_date: :DESC)
+  end
+
+  # Without respond_with
+  # def index
+  #   respond_to do |format|
+  #     format.json { render json: Event.order(event_date: :DESC) }
+  #   end
+  # end
+  
+
+  def show 
+    respond_with Event.find(params[:id])
+  end
+
+  def create
+    respond_with :api, Event.create(event_params)
+  end
+
+  def destroy
+    respond_to Event.destroy(params[:id])
+  end
+
+  def update 
+    event = Event.find(params['id'])
+    event.update(event_params)
+    respond_with Event, json:event
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(
+      :id,
+      :event_type,
+      :event_date,
+      :title,
+      :speaker,
+      :host,
+      :published
+    )
+  end
+end
